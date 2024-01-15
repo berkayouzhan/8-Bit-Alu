4e27b304ef7370e99cefe79ded060e93653b78e0
Berkay Oğuzhan <brky.gzhn1238@gmail.com>
2024-01-15 17:57:10 +0300

    4-bit-alu-verilog

---
+++ 4_bit_alu_verilog/Synthesis/design.sv
@@ -0,0 +1,24 @@
+// design.sv
+`timescale 1ns/1ps
+module ALU (
+    input [3:0] operandA,
+    input [3:0] operandB,
+    input [2:0] operation,
+  	output reg [7:0] result,
+    output reg zeroFlag
+);
+
+always @* begin
+    case(operation)
+        3'b000: result <= operandA + operandB; // Addition
+        3'b001: result <= operandA - operandB; // Subtraction
+        3'b010: result <= operandA & operandB; // Bitwise AND
+        3'b011: result <= operandA | operandB; // Bitwise OR
+        3'b100: result <= operandA * operandB; // Multiplication
+        default: result <= 4'b0000; // Default to 0 for unknown operation
+    endcase
+
+    zeroFlag <= (result == 4'b0000) ? 1 : 0; // Set zero flag
+end
+
+endmodule
---
+++ 4_bit_alu_verilog/Synthesis/testbench.sv
@@ -0,0 +1,47 @@
+// testbench.sv
+`timescale 1ns/1ps
+module testbench;
+
+reg [3:0] operandA, operandB;
+reg [2:0] operation;
+ wire [7:0] result;
+wire zeroFlag;
+
+ALU myALU (
+    .operandA(operandA),
+    .operandB(operandB),
+    .operation(operation),
+    .result(result),
+    .zeroFlag(zeroFlag)
+);
+
+initial begin
+    operandA = 4'b1101;
+    operandB = 4'b1010;
+    operation = 3'b000; // Addition
+    #10;
+
+    operandA = 4'b1101;
+    operandB = 4'b1010;
+    operation = 3'b001; // Subtraction
+    #10;
+
+    operandA = 4'b1101;
+    operandB = 4'b1010;
+    operation = 3'b010; // Bitwise AND
+    #10;
+
+    operandA = 4'b1101;
+    operandB = 4'b1010;
+    operation = 3'b011; // Bitwise OR
+    #10;
+
+    operandA = 4'b1101;
+    operandB = 4'b1010;
+    operation = 3'b100; // Multiplication
+    #10;
+
+    $stop;
+end
+
+endmodule
